777badb5aeecec2729fad1aa431da95d338d9eeb
Mayank <mayanktiwari1212.mt@gmail.com>
2024-11-12 15:06:20 +0530

    feat: Updated config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-type ConfigKeys = "PORT" | "DATABASE_URL" | "SERVER_URL" | "ENV";
+type ConfigKeys = "PORT" | "DATABASE_URL" | "SERVER_URL" | "ENV" | "ADMIN_USERNAME" | "ADMIN_PASSWORD" | "ACCESS_TOKEN_SECRET" | "ACCESS_TOKEN_EXPIRY" | "REFRESH_TOKEN_SECRET" | "REFRESH_TOKEN_EXPIRY" | "DOMAIN";
 
 const _config: Record<ConfigKeys, string | undefined> = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   SERVER_URL: process.env.SERVER_URL,
-  ENV: process.env.ENV
+  ENV: process.env.ENV,
+  ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
+  DOMAIN: process.env.DOMAIN
 };
 
 export const AppConfig = {
