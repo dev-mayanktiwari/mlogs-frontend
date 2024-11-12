17b0ea518db684c7ebbc58e855ca082350cb374b
Mayank <mayanktiwari1212.mt@gmail.com>
2024-11-12 13:11:22 +0530

    feat: Init Setup

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import router from "./router/apiRouter";
+import cors from "cors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
+import { ResponseMessage } from "./constant/responseMessage";
+import httpError from "./utils/httpError";
+import helmet from "helmet";
+
+const app: Application = express();
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/v1", router);
+
+//404 Handler
+app.use((req: Request, _: Response, next: NextFunction) => {
+  try {
+    throw new Error(ResponseMessage.NOT_FOUND);
+  } catch (error) {
+    httpError(next, error, req, 404);
+  }
+});
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+export default app;
+
