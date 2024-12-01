d57fc9e730ffc236ceed79997295fb632e504c08
Mayank <mayanktiwari1212.mt@gmail.com>
2024-12-01 13:35:36 +0530

    feat: Router init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <>Hello World!!</>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Hello, world!</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
